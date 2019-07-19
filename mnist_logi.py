58c429768d84375e2061fe3e26947edc4ee1e43b
ryuto <rryuto5210@gmail.com>
2019-07-19 17:53:04 +0900

    Add mnist

---
+++ mnist_logi.py
@@ -0,0 +1,129 @@
+### パッケージ（os, numpy, PIL, matplotlib）のインポート
+# os: ディレクトリの操作を簡単にするモジュール
+import os
+# numpy(np): 配列計算の効率化モジュール
+import numpy as np
+# pillow(PIL): 画像データ処理のモジュール
+from PIL import Image
+# matplotlib(plt): 画像を表示するためのモジュール
+import matplotlib.pyplot as plt
+
+# sklearnのインポート
+import sklearn
+# 手書き文字のデータ(MNIST)
+from sklearn.datasets import fetch_mldata
+mnist = fetch_mldata('MNIST original')
+# ロジスティック回帰のモジュール(LogisticRegression)
+from sklearn.linear_model import LogisticRegression
+# 教師データの分割をするモジュール(train_test_split)
+from sklearn.model_selection import train_test_split
+
+### 画像データ読み込み、加工
+
+# 画像の入っているフォルダを指定し、中身のファイル名を取得
+# filenames = sorted(os.listdir('handwrite_numbers'))
+filenames = sorted(os.listdir('handwrite2_numbers'))
+# print(filenames)
+# ['eight1.png', 'eight2.png', 'eight3.png', 'five1.png', 'five2.png', 'five3.png', 'four1.png', 'four2.png', 'four3.png', 'nine1.png', 'nine2.png', 'nine3.png', 'one1.png', 'one2.png', 'one3.png', 'seven1.png', 'seven2.png', 'seven3.png', 'six1.png', 'six2.png', 'six3.png', 'three1.png', 'three2.png', 'three3.png', 'two1.png', 'two2.png', 'two3.png', 'zero1.png', 'zero2.png', 'zero3.png']
+
+# フォルダ内の全画像をデータ化
+# np.empty() 要素の値が不定の状態のままで，指定した大きさの配列を生成する関数
+img_test = np.empty((0, 784))
+for filename in filenames:
+    # 画像ファイルを取得、グレースケール（モノクロ）にしてサイズ変更
+    # img = Image.open('handwrite_numbers/' + filename).convert('L')
+
+    img = Image.open('handwrite2_numbers/' + filename).convert('L')
+    # 画像の表示
+    # img.show()
+    resize_img = img.resize((784, 784))
+    # リサイズされていることを確認
+    # resize_img.show()
+
+    # サイズを更に縮めて配列を作り、sklearnのdigitsと同じ型（8 × 8）にする
+    # 通常のpng画像の明度は0〜255なので、サンプルデータに合わせて0〜15に変換
+    img_data256 = np.array([])
+    # 64画素の1画素ずつ明るさをプロット
+    for y in range(28):
+        for x in range(28):
+            # 1画素に縮小される範囲の明るさの二乗平均をとり、白黒反転(sklearnのdigitsに合わせるため)
+            # crop()は、画像の一部の領域を切り抜くメソッド。切り出す領域を引数(left, upper, right, lower)（要は左上と右下の座標）で指定する。
+            crop = np.asarray(resize_img.crop((x * 28, y * 28, x * 28 + 28, y * 28 + 28)))
+            bright = 255 - crop.mean()**2 / 255
+            img_data256 = np.append(img_data256, bright)
+
+    img_data = img_data256 / 255
+    # 画像データ内の最小値が0, 最大値が16になるようになるように計算(sklearnのdigitsに合わせるため)
+    # min_bright = img_data256.min()
+    # max_bright = img_data256.max()
+    # img_data16 = (img_data256 - min_bright) / (max_bright - min_bright) * 16
+
+    # 加工した画像データをmnist_edited_imagesに出力する
+    # cmap='gray'でグレースケールで表示
+    # plt.imshow(img_data.reshape(28, 28), cmap='gray')
+    # plt.savefig("/Users/ryuto/works/judge-num/mnist_edited_numbers/edited_" + filename.replace(".png", "") + ".png")
+
+    img_test = np.r_[img_test, img_data.reshape(1, -1)]
+
+X = mnist.data / 255
+y = mnist.target
+
+#
+# # mnistのデータセットを画像として保存する
+# # リスト（配列）などのイテラブルオブジェクトの要素とインデックス（カウンタ）を同時に取得したい場合は、enumerate()関数を使う。
+# for i, x in enumerate(X[:10]):
+#     plt.imshow(x.reshape(28, 28), cmap='gray')
+#     plt.savefig("/Users/ryuto/works/judge-num/mnist_numbers/mnist_" + str(i) + ".png")
+
+train_size = 500
+test_size = 100
+# # 教師データとテストデータに分ける
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, train_size=train_size, random_state=0)
+# # ロジスティック回帰のモデルを作る。教師データを使って学習
+# # FutureWarning: Default solver will be changed to 'lbfgs' in 0.22. Specify a solver to silence this warning.
+# #   FutureWarning)
+# # solver='liblinear'を追記することで、上記のWarningが出ないようになった。
+#
+# # FutureWarning: Default multi_class will be changed to 'auto' in 0.22. Specify the multi_class option to silence this warning.
+# #   "this warning.", FutureWarning)
+# # 同様に、multi_class='auto'を追記することで、上記のWarningが出ないようになった。
+# # 参考記事： https://machinelearningmastery.com/how-to-fix-futurewarning-messages-in-scikit-learn/
+logreg = LogisticRegression(solver='liblinear', multi_class='auto')
+logreg_model = logreg.fit(X_train, y_train)
+
+print("教師データのスコア：", logreg_model.score(X_train, y_train))
+print("テストデータのスコア：", logreg_model.score(X_test, y_test))
+
+# 画像データの正解を配列にしておく
+X_true = []
+for filename in filenames:
+    X_true = X_true + [int(filename[:1])]
+
+X_true = np.array(X_true)
+print(X_true)
+
+pred_logreg = logreg_model.predict(img_test)
+
+# 結果の出力
+print("判定結果")
+print("観測：", X_true)
+print("予測：", pred_logreg)
+print("正答率：", logreg_model.score(img_test, X_true))
+#
+# ## handwrite_numbers
+# # 教師データのスコア： 0.9988864142538976
+# # テストデータのスコア： 0.9443826473859844
+# # [0 0 0 1 1 1 2 2 2 3 3 3 4 4 4 5 5 5 6 6 6 7 7 7 8 8 8 9 9 9]
+# # 判定結果
+# # 観測： [0 0 0 1 1 1 2 2 2 3 3 3 4 4 4 5 5 5 6 6 6 7 7 7 8 8 8 9 9 9]
+# # 予測： [1 1 1 8 1 1 4 8 4 4 1 1 1 1 1 1 1 1 6 1 6 7 1 1 6 1 1 4 4 1]
+# # 正答率： 0.16666666666666666
+#
+# ## handwrite2_numbers
+# # 教師データのスコア： 0.9988864142538976
+# # テストデータのスコア： 0.9443826473859844
+# # [0 0 0 1 1 1 2 2 2 3 3 3 4 4 4 5 5 5 6 6 6 7 7 7 8 8 8 9 9 9]
+# # 判定結果
+# # 観測： [0 0 0 1 1 1 2 2 2 3 3 3 4 4 4 5 5 5 6 6 6 7 7 7 8 8 8 9 9 9]
+# # 予測： [0 0 0 6 1 1 2 2 2 3 6 9 6 4 7 5 5 1 5 4 4 7 1 7 9 5 5 5 9 9]
+# # 正答率： 0.5333333333333333
