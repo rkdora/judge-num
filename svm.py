1a91a81cd512613f748e307c71ed215adf561e3b
ryuto <rryuto5210@gmail.com>
2019-07-29 08:26:27 +0900

    Add timeit for svm.py

--- svm.py
+++ svm.py
@@ -18,6 +18,9 @@ from sklearn import svm
 # 参考文献では、`from sklearn.cross_validation import train_test_split`だったが、現在は以下が正しい
 from sklearn.model_selection import train_test_split
 
+# timeit: 処理時間を測定するためのモジュール
+import timeit
+
 ### 画像データ読み込み、加工
 
 # 画像の入っているフォルダを指定し、中身のファイル名を取得
@@ -94,18 +97,16 @@ y = digits.target
 #     plt.savefig("/home/ryuto/judge-num/sklearn_numbers/sklearn_" + str(i) + ".png")
 
 # 教師データとテストデータに分ける
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=0)
-# ロジスティック回帰のモデルを作る。教師データを使って学習
-# logreg = LogisticRegression(solver='liblinear', multi_class='auto')
-# logreg_model = logreg.fit(X_train, y_train)
+train_size = 1000
+test_size = 700
+X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=train_size, test_size=test_size, random_state=0)
 
 # サポートベクターマシンのモデルを作る
 clf = svm.SVC(gamma=0.001)
-svm_model = clf.fit(X_train, y_train)
-
-
-print("教師データのスコア：", svm_model.score(X_train, y_train))
-print("テストデータのスコア：", svm_model.score(X_test, y_test))
+loop_count = 10
+print("学習時間：", timeit.timeit(lambda: clf.fit(X_train, y_train), number=loop_count) / loop_count)
+print("教師データのスコア：", clf.score(X_train, y_train))
+print("テストデータのスコア：", clf.score(X_test, y_test))
 
 # 画像データの正解を配列にしておく
 X_true = []
@@ -113,17 +114,16 @@ for filename in filenames:
     X_true = X_true + [int(filename[:1])]
 
 X_true = np.array(X_true)
-print(X_true)
 
-pred_svm = svm_model.predict(img_test)
+pred_svm = clf.predict(img_test)
 
 # 結果の出力
 print("判定結果")
 print("観測：", X_true)
 print("予測：", pred_svm)
-print("正答率：", svm_model.score(img_test, X_true))
+print("正答率：", clf.score(img_test, X_true))
 
-### サポートベクターマシン
+### test_size = 0.5
 # 教師データのスコア： 0.9988864142538976
 # テストデータのスコア： 0.9899888765294772
 ## handwrite_numbers
@@ -143,3 +143,23 @@ print("正答率：", svm_model.score(img_test, X_true))
 # 観測： [0 0 0 1 1 1 2 2 2 3 3 3 4 4 4 5 5 5 6 6 6 7 7 7 8 8 8 9 9 9]
 # 予測： [4 4 0 1 1 1 2 2 2 9 9 4 4 4 4 5 4 5 4 4 4 7 7 7 8 4 8 4 9 9]
 # 正答率： 0.6333333333333333
+
+### train_size = 1000, test_size = 700
+# 学習時間： 0.06385449170000002
+# 教師データのスコア： 0.998
+# テストデータのスコア： 0.9928571428571429
+## handwrite_numbers
+# 判定結果
+# 観測： [0 0 0 1 1 1 2 2 2 3 3 3 4 4 4 5 5 5 6 6 6 7 7 7 8 8 8 9 9 9]
+# 予測： [4 4 4 1 1 1 4 4 4 9 1 1 4 4 4 4 4 4 4 4 4 4 4 1 4 4 1 1 4 1]
+# 正答率： 0.2
+## handwrite2_numbers
+# 判定結果
+# 観測： [0 0 0 1 1 1 2 2 2 3 3 3 4 4 4 5 5 5 6 6 6 7 7 7 8 8 8 9 9 9]
+# 予測： [1 1 0 1 1 1 2 2 2 3 9 9 4 4 7 5 9 4 5 4 5 7 7 7 8 9 9 8 9 7]
+# 正答率： 0.5333333333333333
+## handwrite3_numbers
+# 判定結果
+# 観測： [0 0 0 1 1 1 2 2 2 3 3 3 4 4 4 5 5 5 6 6 6 7 7 7 8 8 8 9 9 9]
+# 予測： [4 4 0 1 1 1 2 2 2 9 9 4 4 4 4 5 4 5 4 4 4 7 7 7 8 1 8 9 9 9]
+# 正答率： 0.6666666666666666
